3bcf3b3203ddcb7556c48f772f0eb49baafc4c3c
Jayleh <jaylehyun@gmail.com>
2018-05-09 18:02:19 -0700

    added default gauge meter

--- static/index.js
+++ static/index.js
@@ -112,6 +112,87 @@ function pieChart(sample_id) {
 }
 
 
+function gaugeChart(sample_id) {
+    let wfreqUrl = `/wfreq/${sample_id}`;
+
+    Plotly.d3.json(wfreqUrl, (error, wfreqData) => {
+        if (error) return console.warn(error);
+
+        console.log(wfreqData);
+
+        // Enter a speed between 0 and 180
+        let level = 175;
+
+        // Trig to calc meter point
+        let degrees = 180 - level,
+            radius = .5;
+        let radians = degrees * Math.PI / 180;
+        let x = radius * Math.cos(radians);
+        let y = radius * Math.sin(radians);
+
+        // Path: may have to change to create a better triangle
+        let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        let path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+        let gaugeData = [{
+            type: 'scatter',
+            x: [0], y: [0],
+            marker: { size: 28, color: '850000' },
+            showlegend: false,
+            name: 'speed',
+            text: level,
+            hoverinfo: 'text+name'
+        },
+        {
+            values: [50 / 6, 50 / 6, 50 / 6, 50 / 6, 50 / 6, 50 / 6, 50],
+            rotation: 90,
+            text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
+                'Slow', 'Super Slow', ''],
+            textinfo: 'text',
+            textposition: 'inside',
+            marker: {
+                colors: ['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                    'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                    'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                    'rgba(255, 255, 255, 0)']
+            },
+            labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+            hoverinfo: 'label',
+            hole: .5,
+            type: 'pie',
+            showlegend: false
+        }];
+
+        let gaugeLayout = {
+            shapes: [{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                }
+            }],
+            height: 400,
+            width: 400,
+            xaxis: {
+                zeroline: false, showticklabels: false,
+                showgrid: false, range: [-1, 1]
+            },
+            yaxis: {
+                zeroline: false, showticklabels: false,
+                showgrid: false, range: [-1, 1]
+            }
+        };
+
+        Plotly.react('gauge', gaugeData, gaugeLayout);
+    });
+}
+
+
 function bubbleChart(sample_id) {
     let samplesUrl = `/samples/${sample_id}`;
 
@@ -177,16 +258,20 @@ function bubbleChart(sample_id) {
 function optionChanged(sample_id) {
     // console.log(sample_id);
 
+    let defaultSample = 'BB_940';
+
     switch (sample_id) {
         case sample_id:
             sampleTable(sample_id);
             pieChart(sample_id);
             bubbleChart(sample_id);
+            gaugeChart(sample_id);
             break;
         default:
-            sampleTable("BB_940");
-            pieChart("BB_940");
-            bubbleChart("BB_940");
+            sampleTable(defaultSample);
+            pieChart(defaultSample);
+            bubbleChart(defaultSample);
+            gaugeChart(defaultSample);
             break;
     }
 }
@@ -194,9 +279,13 @@ function optionChanged(sample_id) {
 
 function init() {
     dropDown();
-    sampleTable('BB_940');
-    pieChart('BB_940');
-    bubbleChart('BB_940');
+
+    let defaultSample = 'BB_940';
+
+    sampleTable(defaultSample);
+    pieChart(defaultSample);
+    bubbleChart(defaultSample);
+    gaugeChart(defaultSample);
 }
 
 
